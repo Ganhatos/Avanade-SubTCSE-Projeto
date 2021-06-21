2c661a50f7d0b1a70d3d0b4754de043895ef0e93
Ganhatos <ganhatos@gmail.com>
2021-06-21 09:39:18 -0300

    aula 5

---
+++ src/Avanade.SubTCSE.Projeto.Application/AutoMapperConfigs/Profiles/EmployeeRole/EmployeeRoleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Avanade.SubTCSE.Projeto.Application.AutoMapperConfigs.Profiles.EmployeeRole
+{
+    public class EmployeeRoleProfile : Profile
+    {
+        public EmployeeRoleProfile()
+        {
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDto, Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+                .ForCtorParam("roleName", opt => opt.MapFrom(src => src.Cargo));
+
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDto, Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+               .ForMember
+        }
+    }
+}
---
+++ src/Avanade.SubTCSE.Projeto.Application/Interfaces/EmployeeRole/IEmployeeRoleAppService.cs
@@ -0,0 +1,9 @@
+﻿using Avanade.SubTCSE.Projeto.Application.Dto.EmployeeRole;
+
+namespace Avanade.SubTCSE.Projeto.Application.Interfaces.EmployeeRole
+{
+    public interface IEmployeeRoleAppService
+    {
+        void AddEmployeeRole(EmployeeRoleDto employeeRoleDto);
+    }
+}
